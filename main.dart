cf573d2fb5dce366a53fa1503cd31efc37e36cd0
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2021-09-29 21:44:19 +0530

    Simple text provider

---
+++ main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+final textProvider = Provider((_) => "Hello Provider in a previous version.");
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Consumer(builder: (context, watch, _) {
+            final String val = watch(textProvider);
+            return Text(val);
+          }),
+        ),
+      ),
+    );
+  }
+}
