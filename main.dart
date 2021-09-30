f3c81f979598e8a33dac62e73aaa2220cacb016b
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2021-09-30 08:56:24 +0530

    Change Notifier used.

--- main.dart
+++ main.dart
@@ -9,25 +9,78 @@ void main() {
   );
 }
 
-final textProvider = Provider((_) => "Hello Provider in a previous version.");
+final textProvider = Provider((ref) => "Hello Provider in a previous version.");
+final visibilityProvider = ChangeNotifierProvider((ref) => Visible());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Consumer(builder: (context, watch, _) {
+              final String val = watch(textProvider);
+              final bool isVisible = watch(visibilityProvider).isVisible;
+              return Visibility(
+                visible: isVisible,
+                child: Text(val),
+              );
+            }),
+          ),
+          ElevatedButton(
+              onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SecondScreen(),
+                    ),
+                  ),
+              child: const Text('Go to next screen'))
+        ],
       ),
-      home: Scaffold(
-        body: Center(
-          child: Consumer(builder: (context, watch, _) {
-            final String val = watch(textProvider);
-            return Text(val);
-          }),
-        ),
+    );
+  }
+}
+
+class Visible extends ChangeNotifier {
+  bool isVisible = true;
+
+  void makeInvisible() {
+    isVisible = !isVisible;
+    notifyListeners();
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Consumer(builder: (context, watch, _) {
+          final isVisible = watch(visibilityProvider.notifier);
+          return ElevatedButton(
+              onPressed: () {
+                isVisible.makeInvisible();
+                Navigator.pop(context);
+              },
+              child: const Text('Make inVisible'));
+        }),
       ),
     );
   }
